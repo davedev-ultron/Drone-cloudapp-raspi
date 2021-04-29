f66c2a5bc47cd1a828a394f66582fb813a3c5d28
Gerardo Calvillo <gerry@Gerardos-MacBook-Pro.local>
2021-04-28 22:13:14 -0500

    Functioning video_stream.py

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 #Libraries to install on raspi
 
-sudo apt update sudo apt install libhdf5-dev sudo apt install libhdf5-serial-dev sudo apt install libatlas-base-dev sudo apt install libjasper-dev sudo apt install libqtgui4 sudo apt install libqt4-test sudo apt install python3-opencv
+sudo apt update
+
+sudo apt install libhdf5-serial-dev
+sudo apt install libatlas-base-dev
+sudo apt install libjasper-dev
+sudo apt install libqtgui4
+sudo apt install libqt4-test
+sudo apt install python3-opencv
 
 sudo pip3 install netifaces psutil google-api-python-client 
-wiringpi dronekit opencv-python
+                  wiringpi dronekit opencv-python
 
 Create logs/ directory in the root app folder on Raspberry Pi (same place where app.py is located)
 
--- video_streamer copy.py
+++ video_streamer copy.py
@@ -1,10 +1,9 @@
 # this file is to read frames from camera and send to cloudapp
 # making video into its own file here for resiliency
 import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
 import time, socket, logging, configparser, argparse, sys
-import utils from Utils
+from utils import Utils
+import numpy as np
 
 parser = argparse.ArgumentParser()
 # if no --d parameter is passed in, the it was started from console, if it is then started as a service
@@ -54,17 +53,19 @@ video_socket = None
 while(True):
     try:
         # setting up camera
-        camera = PiCamera()
-        camera.resolution = (WIDTH, HEIGHT)
-        camera.framerate = FRAMES_PER_SECOND
+        camera = cv2.VideoCapture(0)
         
-        # array to capture data
-        rawCapture = PiRGBArray(camera, size=(WIDTH, HEIGHT))
+        camera.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+        camera.set(cv2.CAP_PROP_FPS, FRAMES_PER_SECOND)
         
         # allowing time for next loop to connect and initialize
         time.sleep(0.1)
         
         logging.info("Camera module initiated")
+        
+        ret, frame = camera.read()
+        logging.info('Actual Width: %s, Actual Height: %s', str(frame.shape[1]), str(frame.shape[0]))
         
         # create sockets
         # SOCK_DGRAM we create datagram which is UDP
@@ -74,19 +75,19 @@ while(True):
         # FOR CONTROLS it is important but not here
         video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         video_socket.connect((HOST_IP, VIDEO_PORT))
-        
         logging.info("Socket Opened, Video Streaming is started")
         
-        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            image_data = frame.array
-            
+        while True:
+            ret, frame = camera.read()
+            image_data = np.array(frame)
+
             # rotate image because cam is mounded upside down
-            image_data = cv2.rotate(image_data, cv2.ROTATE_180)
-            
+            # image_data = cv2.rotate(image_data, cv2.ROTATE_180)
+
             # if network is slow grayscale will speed up
             if GRAYSCALE:
                 image_data = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
-            
+
             # encoding into jpeg
             code, jpg_buffer = cv2.imencode(".jpg", image_data, [int(cv2.IMWRITE_JPEG_QUALITY), JPEG_QUALITY])
 
@@ -94,15 +95,18 @@ while(True):
             # and now it can be sent over network as udp message
             datagramMsgBytes = Utils.create_datagram_message(DRONE_ID, jpg_buffer)
 
-            #sending data
+            # Size was 13338 at width 640, height 480, grayscale false, quality 10
+            # Size was 34438 at width 640, height 480, grayscale false, quality 60
+            # Size was 72394 at width 640, height 480, grayscale false, quality 90, and message is too long
+            #logging.debug('UDP Package size: %s', str(sys.getsizeof(datagramMsgBytes)))
+            # sending data
             video_socket.sendall(datagramMsgBytes)
-            
-            #clearing data for next iteration
-            rawCapture.truncate(0)
-        
+
     except Exception as e:
+        logging.error(e.__traceback__)
+        logging.error(e.__cause__)
         logging.error("Video Stream Ended: " + str(e))
         
         # release the resource
         if camera != None:
-            camera.close()
+            camera.release()
